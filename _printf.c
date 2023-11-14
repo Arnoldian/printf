2a9619300600fa6823f21b309260d975a0645e19
Nkosikhona <nkosikhonadlamini@outlook.com>
2023-11-14 08:26:16 -0800

    task 1

--- _printf.c
+++ _printf.c
@@ -2,60 +2,93 @@
 #include <stdarg.h>
 #include <unistd.h>
 
-int write_str(char *str);
-int write_int(int n);
-int _strlen(char *s);
-char *itoa(int n, char *buffer);
-char *reverse_str(char *str);
+#define BUFFER_SIZE 20
+
+/**
+ * write_char - Helper func to write a char
+ * @c: Char to write
+ * Return: Num of chars written
+ */
+int write_char(int c) {
+    return write(1, &c, 1);
+}
+
+/**
+ * write_str - Helper func to write a str with precision
+ * @str: Str to write
+ * @precision: Max chars to write
+ * Return: Num of chars written
+ */
+
+int write_str(char *str, int precision) {
+    int count = 0;
+    while (*str && count < precision)
+        write(1, str++, 1), count++;
+    return count;
+}
+
+/**
+ * write_int - Helper func to write an int
+ * @n: Int to write
+ * Return: Number of chars written
+ */
+
+int write_int(int n) {
+    char buffer[BUFFER_SIZE];
+    int i = 0;
+
+    if (n < 0) write_char('-'), n = -n;
+    if (n == 0) return write_char('0');
+
+    while (n != 0) buffer[i++] = '0' + n % 10, n /= 10;
+
+    while (--i >= 0) write(1, &buffer[i], 1);
+    return i;
+}
 
 /**
  * _printf - Custom printf func
- * @format: Format string
+ * @format: Format str
  * Return: Number of chars printed (excl null byte)
  */
 
-int _printf(const char *format, ...)
-{
+int _printf(const char *format, ...) {
     va_list args;
-    int count = 0;
-    const char *ptr;
+    int count = 0, int_arg;
+    char *str_arg;
 
     va_start(args, format);
 
-    for (ptr = format; *ptr != '\0'; ptr++)
-    {
-        if (*ptr != '%')
-        {
-            write(1, ptr, 1);
+    for (; *format; ++format) {
+        if (*format != '%') {
+            write_char(*format);
             count++;
-        }
-        else
-        {
-            ptr++; /* Move to the character after '%' */
-            switch (*ptr)
-            {
-                case 'c':
-                    count += write(1, &va_arg(args, int), 1);
-                    break;
-                case 's':
-                    count += write_str(va_arg(args, char *));
-                    break;
-                case '%':
-                    count += write(1, "%", 1);
-                    break;
-                case 'd':
-                case 'i':
-                    count += write_int(va_arg(args, int));
-                    break;
-                default:
-                    write(1, "%", 1);
-                    write(1, &(*ptr), 1);
-                    count += 2;
+        } else {
+            format++;
+            switch (*format) {
+            case 'c':
+                count += write_char(va_arg(args, int));
+                break;
+            case 's':
+                str_arg = va_arg(args, char *);
+                count += write_str(str_arg ? str_arg : "(null)");
+                break;
+            case '%':
+                count += write_char('%');
+                break;
+            case 'd': case 'i':
+                int_arg = va_arg(args, int);
+                count += write_int(int_arg);
+                break;
+            default:
+                write_char('%');
+                write_char(*format);
+                count += 2;
             }
         }
     }
 
     va_end(args);
-
     return count;
 }
+
