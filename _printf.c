e654f43239a9df5d9b2891926ec9725354b9dede
Nkosikhona <nkosikhonadlamini@outlook.com>
2023-11-14 08:11:41 -0800

    task 0 and 1 (1 is only mendatory)

--- _printf.c
+++ _printf.c
@@ -2,18 +2,25 @@
 #include <stdarg.h>
 #include <unistd.h>
 
+int write_str(char *str);
+int write_int(int n);
+int _strlen(char *s);
+char *itoa(int n, char *buffer);
+char *reverse_str(char *str);
+
 /**
- * _printf - Custom printf function
+ * _printf - Custom printf func
  * @format: Format string
- * Return: Number of characters printed (excluding null byte)
+ * Return: Number of chars printed (excl null byte)
  */
+
 int _printf(const char *format, ...)
 {
-    va_list args_list;
+    va_list args;
     int count = 0;
     const char *ptr;
 
-    va_start(args_list, format);
+    va_start(args, format);
 
     for (ptr = format; *ptr != '\0'; ptr++)
     {
@@ -28,17 +35,17 @@ int _printf(const char *format, ...)
             switch (*ptr)
             {
                 case 'c':
-                    count += write(1, va_arg(args_list, int), 1);
+                    count += write(1, &va_arg(args, int), 1);
                     break;
                 case 's':
-                    count += write(1, va_arg(args_list, char *), 1);
+                    count += write_str(va_arg(args, char *));
                     break;
                 case '%':
                     count += write(1, "%", 1);
                     break;
                 case 'd':
                 case 'i':
-                    count += write(1, va_arg(args_list, int), 1);
+                    count += write_int(va_arg(args, int));
                     break;
                 default:
                     write(1, "%", 1);
@@ -48,8 +55,7 @@ int _printf(const char *format, ...)
         }
     }
 
-    va_end(args_list);
+    va_end(args);
 
-    return (count);
+    return count;
 }
-
